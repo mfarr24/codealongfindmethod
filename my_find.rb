78b91b31626c2a3691d87bba069e885022b1a809
mfarr24 <77256268+mfarr24@users.noreply.github.com>
2021-01-22 19:21:14 -0500

    Add files via upload

---
+++ my_find.rb
@@ -0,0 +1,27 @@
+r#equire 'pry'
+
+
+    #def my_find(collection)
+        #i = 0
+        #while i < collection.length
+          #i = i + 1
+        #end
+      #end 
+#end
+
+#def my_find(collection)
+    #i = 0
+    #while i < collection.length
+      #if yield(collection[i])
+        #return collection[i]
+      #end
+      #i = i + 1
+    #end
+  
+    def my_find(collection)
+        i = 0
+        while i < collection.length
+          return collection[i] if yield(collection[i])
+          i = i + 1
+        end
+      end 
